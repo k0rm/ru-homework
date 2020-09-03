e1561639e80999096871c626f8453bb9f892c322
k0rm <kormakur20ru.is>
2020-09-03 10:25:02 +0000

    Algorithim written for max_int

---
+++ max_int.py
@@ -0,0 +1,17 @@
+# Code takes an integer input
+# Then a while loop starts as long as the input is positive it keeps looping
+# Save the highest number inside a variable so that you can keep the highest value 
+# If the input value is negative then break the loop
+# Print the highest value
+
+num_int = int(input("Input a number: "))
+max_int = 0
+highest_value = 0
+while num_int > 0:
+    num_int = int(input("Input a number: "))
+    blonk = num_int
+    if blonk >= highest_value
+        highest_value = blonk
+    if num_int < 0:
+        break
+print("The maximum is", max_int)
